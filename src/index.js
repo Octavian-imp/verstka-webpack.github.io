6ada7b3bd829642cc3eee54c460a768fa03383d6
Octavian_Jr <sirghi1979@mail.ru>
2024-10-01 20:02:54 +0300

    блок 1.5 JS в браузере. добавлен слайдер

--- src/index.js
+++ src/index.js
@@ -1,4 +1,64 @@
+import Swiper from "swiper";
+import { FreeMode, Pagination } from "swiper/modules";
+
 import "@/index.scss";
+
+const swiperOptions = {
+  modules: [FreeMode, Pagination],
+  spaceBetween: 16,
+  freeMode: true,
+  width: 240,
+  pagination: { clickable: true, el: ".swiper-pagination" },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+  },
+  on: {
+    resize: initBrandsSlider,
+  },
+};
+
+let swiperBrands = new Swiper(".brands-slider", swiperOptions);
+function initBrandsSlider() {
+  if (!swiperBrands && window.innerWidth < 768) {
+    swiperBrands = new Swiper(".brands-slider", swiperOptions);
+  }
+  if (swiperBrands && window.innerWidth >= 768) {
+    swiperBrands.destroy(true, true);
+    swiperBrands = null;
+  }
+}
+
+window.addEventListener("resize", initBrandsSlider);
+
+const brandsSliderWrapperSelector = "brands-slider__wrapper";
+const brandsSliderWrapperActiveSelector = "brands-slider__wrapper--active";
+
+const moreButtonSelector = "brands-slider__more-button";
+const moreButtonActiveSelector = "brands-slider__more-button--active";
+
+const brandsSliderWrapper = document.querySelector(
+  "." + brandsSliderWrapperSelector
+);
+
+const moreButton = document.getElementById(moreButtonSelector);
+
+moreButton.addEventListener("click", () => {
+  if (
+    brandsSliderWrapper.classList.contains(brandsSliderWrapperActiveSelector)
+  ) {
+    brandsSliderWrapper.classList.remove(brandsSliderWrapperActiveSelector);
+
+    moreButton.querySelector("span").textContent = "Показать все";
+    moreButton.classList.remove(moreButtonActiveSelector);
+  } else {
+    brandsSliderWrapper.classList.add(brandsSliderWrapperActiveSelector);
+
+    moreButton.classList.add(moreButtonActiveSelector);
+    moreButton.querySelector("span").textContent = "Скрыть все";
+  }
+});
 
 const menuClassActive = "mob-menu--active";
 const burgerBtn = document.getElementById("menuBurger");
