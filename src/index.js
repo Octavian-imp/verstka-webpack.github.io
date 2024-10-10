4803df71fac71ea9f5dc5cc33acf7a14e0cc3557
Octavian_Jr <sirghi1979@mail.ru>
2024-10-10 18:24:42 +0300

    добавлен dotenv для чтения env файла. Сделан финальный макет. удален tailwind.

--- src/index.js
+++ src/index.js
@@ -1,78 +1,152 @@
-import Swiper from "swiper";
-import { FreeMode, Pagination } from "swiper/modules";
+import Swiper from "swiper"
+import { FreeMode, Pagination } from "swiper/modules"
 
-import "@/index.scss";
-import { createIcons, icons } from "lucide";
+import "@/index.scss"
+import { createIcons, icons } from "lucide"
 
-createIcons({ icons });
+createIcons({ icons })
 
-const swiperOptions = {
-  modules: [FreeMode, Pagination],
-  spaceBetween: 16,
-  freeMode: true,
-  width: 240,
-  pagination: { clickable: true, el: ".swiper-pagination" },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-  },
-  on: {
-    resize: initBrandsSlider,
-  },
-};
+// слайдеры
 
-let swiperBrands = new Swiper(".brands-slider", swiperOptions);
-function initBrandsSlider() {
-  if (!swiperBrands && window.innerWidth < 768) {
-    swiperBrands = new Swiper(".brands-slider", swiperOptions);
-  }
-  if (swiperBrands && window.innerWidth >= 768) {
-    swiperBrands.destroy(true, true);
-    swiperBrands = null;
-  }
+let swiper = {
+  // active: true только когда все слайдеры инициализированы
+  active: false,
+  sliders: [],
 }
 
-window.addEventListener("resize", initBrandsSlider);
+function initSliders() {
+  document.querySelectorAll(".swiper-container").forEach((el, indx) => {
+    swiper.active = window.innerWidth < 768 && true
+    swiper.sliders.push(
+      new Swiper(el, {
+        modules: [FreeMode, Pagination],
+        spaceBetween: 16,
+        freeMode: true,
+        width: 240,
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+          },
+        },
+        on: {
+          resize: () => {
+            if (window.innerWidth >= 768) {
+              if (swiper.sliders[indx] !== null) {
+                swiper.sliders[indx].destroy(true, true)
+                swiper.sliders[indx] = null
+              }
+            }
+          },
+        },
+        pagination: {
+          clickable: true,
+          el: el.querySelector(".swiper-pagination"),
+        },
+      })
+    )
+  })
+}
 
-const brandsSliderWrapperSelector = "brands-slider__wrapper";
-const brandsSliderWrapperActiveSelector = "brands-slider__wrapper--active";
+initSliders()
 
-const moreButtonSelector = "brands-slider__more-button";
-const moreButtonActiveSelector = "brands-slider__more-button--active";
-
-const brandsSliderWrapper = document.querySelector(
-  "." + brandsSliderWrapperSelector
-);
-
-const moreButton = document.getElementById(moreButtonSelector);
-
-moreButton.addEventListener("click", () => {
-  if (
-    brandsSliderWrapper.classList.contains(brandsSliderWrapperActiveSelector)
-  ) {
-    brandsSliderWrapper.classList.remove(brandsSliderWrapperActiveSelector);
-
-    moreButton.querySelector("span").textContent = "Показать все";
-    moreButton.classList.remove(moreButtonActiveSelector);
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    if (swiper.active) {
+      swiper.sliders = swiper.sliders.map((el) => {
+        if (el !== null) {
+          el.destroy(true, true)
+        }
+        return null
+      })
+      swiper.active = false
+    }
   } else {
-    brandsSliderWrapper.classList.add(brandsSliderWrapperActiveSelector);
-
-    moreButton.classList.add(moreButtonActiveSelector);
-    moreButton.querySelector("span").textContent = "Скрыть все";
+    initSliders()
   }
-});
+})
 
-const menuClassActive = "mob-menu--active";
-const burgerBtn = document.getElementById("menuBurger");
-const menuBtn = document.getElementById("mob-menu");
-const closeBtn = document.getElementById("mob-menu__close");
+// кнопки "показать все"
+const slidersWrapperSelectors = [
+  {
+    defaultClass: "technics-slider__wrapper",
+    activeClass: "technics-slider__wrapper--active",
+    idBtn: "brands-slider__more-button",
+    activeBtnClass: "more-button--active",
+  },
+  {
+    defaultClass: "type-technics-slider__wrapper",
+    activeClass: "type-technics-slider__wrapper--active",
+    idBtn: "type-technics-slider__more-button",
+    activeBtnClass: "more-button--active",
+  },
+]
+
+slidersWrapperSelectors.forEach((el) => {
+  const btn = document.getElementById(el.idBtn)
+  btn.addEventListener("click", () => {
+    if (
+      document
+        .querySelector("." + el.defaultClass)
+        .classList.contains(el.activeClass)
+    ) {
+      document
+        .querySelector("." + el.defaultClass)
+        .classList.remove(el.activeClass)
+      btn.classList.remove(el.activeBtnClass)
+      btn.querySelector("span").textContent = "Показать все"
+    } else {
+      document
+        .querySelector("." + el.defaultClass)
+        .classList.add(el.activeClass)
+      btn.classList.add(el.activeBtnClass)
+      btn.querySelector("span").textContent = "Скрыть все"
+    }
+  })
+})
+
+// мобильное меню
+const menuClassActive = "responsive-menu--active"
+const burgerBtn = document.getElementById("menuBurger")
+const menuBtn = document.getElementById("mob-menu")
+const closeBtn = document.getElementById("responsive-menu__close")
 
 burgerBtn.addEventListener("click", (e) => {
-  e.stopPropagation();
-  menuBtn.classList.add(menuClassActive);
-});
+  e.stopPropagation()
+  menuBtn.classList.add(menuClassActive)
+  document.body.style.overflowY = "hidden"
+})
 
 closeBtn.addEventListener("click", () => {
-  menuBtn.classList.remove(menuClassActive);
-});
+  menuBtn.classList.remove(menuClassActive)
+  document.body.style.overflowY = "auto"
+})
+
+// модалки
+// для работы нужно кнопке добавить класс btn-open-modal и атрибут data-modal-id с id модального окна
+
+const btnOpenModalSelectorClass = "btn-open-modal"
+const modalActiveClass = "modal__wrapper--active"
+const btnCloseModalSelectorClass = "modal__btn-close"
+
+document.querySelectorAll("." + btnOpenModalSelectorClass).forEach((el) => {
+  el.addEventListener("click", (e) => {
+    document.body.style.overflowY = "hidden"
+    const modalId = e.target.dataset.modalId
+    const modal = document.getElementById(modalId)
+    modal.classList.add(modalActiveClass)
+    function closeModal() {
+      document.body.style.overflowY = "auto"
+      const timingAnimation = 200
+      modal.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: timingAnimation,
+        easing: "ease-out",
+      })
+      setTimeout(() => {
+        modal.classList.remove(modalActiveClass)
+      }, timingAnimation)
+    }
+    modal
+      .querySelector("." + btnCloseModalSelectorClass)
+      .addEventListener("click", closeModal, { once: true })
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserWebpackPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const TerserWebpackPlugin = require("terser-webpack-plugin")
+const webpack = require("webpack")
+const dotenv = require("dotenv").config({
+  path: path.join(__dirname, ".env"),
+})
 
-const isDev = process.env.MODE === "development" || process.env.MODE === "dev";
-const PORT = process.env.PORT || 3000;
-const MODE_DEV = isDev ?? "development";
+const isDev = process.env.MODE === "development" || process.env.MODE === "dev"
+const PORT = process.env.PORT || 3000
+const MODE_DEV = isDev ?? "development"
 
 /**
  * Returns babel options with provided presets. If no presets are provided,
@@ -19,13 +23,13 @@ const MODE_DEV = isDev ?? "development";
 function getBabelOpts(...preset) {
   const opts = {
     presets: ["@babel/preset-env"],
-  };
-
-  if (preset.length > 0) {
-    opts.presets.push(...preset);
   }
 
-  return opts;
+  if (preset.length > 0) {
+    opts.presets.push(...preset)
+  }
+
+  return opts
 }
 
 /**
@@ -39,12 +43,12 @@ function getOptimization() {
     splitChunks: {
       chunks: "all",
     },
-  };
+  }
 
   if (!isDev) {
-    config.minimizer = [new TerserWebpackPlugin()];
+    config.minimizer = [new TerserWebpackPlugin()]
   }
-  return config;
+  return config
 }
 
 /**
@@ -54,17 +58,20 @@ function getOptimization() {
  * @return {array} - Array of style loaders.
  */
 function getStyleLoaders(...preset) {
-  const config = ["style-loader", "css-loader", "postcss-loader"];
+  const config = [
+    isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+    "css-loader",
+    "postcss-loader",
+  ]
 
   if (preset.findIndex((ext) => ext === "scss") !== -1) {
-    config.push("sass-loader");
+    config.push("sass-loader")
   }
-  return config;
+  return config
 }
 
 /**
  * Returns an object containing an array of module rules for webpack.
- * If "tailwind" is included in the addSupportFiles array, it adds support for tailwindcss.
  * If "scss" is included, it adds support for scss/sass files.
  * If "xml" is included, it adds support for xml files.
  * If "csv" is included, it adds support for csv files.
@@ -101,29 +108,29 @@ function getModuleRules(...addSupportFiles) {
         ],
       },
     ],
-  };
+  }
 
   if (addSupportFiles.findIndex((ext) => ext === "scss") !== -1) {
-    const presets = ["scss"];
+    const presets = ["scss"]
 
     config.rules.push({
       test: /\.s[ac]ss$/,
       use: getStyleLoaders(...presets),
-    });
+    })
   }
 
   if (addSupportFiles.findIndex((ext) => ext === "xml") !== -1) {
     config.rules.push({
       test: /\.xml$/,
       use: ["xml-loader"],
-    });
+    })
   }
 
   if (addSupportFiles.findIndex((ext) => ext === "csv") !== -1) {
     config.rules.push({
       test: /\.csv$/,
       use: ["csv-loader"],
-    });
+    })
   }
 
   if (addSupportFiles.findIndex((ext) => ext === "ts") !== -1) {
@@ -136,7 +143,7 @@ function getModuleRules(...addSupportFiles) {
           options: getBabelOpts("ts"),
         },
       ],
-    });
+    })
   }
   if (addSupportFiles.findIndex((ext) => ext === "react") !== -1) {
     config.rules.push({
@@ -148,10 +155,10 @@ function getModuleRules(...addSupportFiles) {
           options: getBabelOpts("@babel/preset-react"),
         },
       ],
-    });
+    })
   }
 
-  return config;
+  return config
 }
 
 module.exports = {
@@ -203,6 +210,12 @@ module.exports = {
         },
       ],
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": dotenv.parsed,
+    }),
   ],
   module: getModuleRules("scss"),
-};
+}
